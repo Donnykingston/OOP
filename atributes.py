2b586b07bdc992c2f6f8989078cdc482104d1463
DonnyKingston <donaldodibeze77@gmail.com>
2022-07-29 08:55:29 +0100

    practiced static methods oop

--- atributes.py
+++ atributes.py
@@ -1,11 +1,13 @@
 
 class Person:
     number_of_people = 0
+    GRAVITY = -9.8
     def __init__(self,name):
         self.name = name
+        Person.add_person()
 
     @classmethod    
-    def number_of_people(cls):
+    def number_of_people_(cls):
         return cls.number_of_people
 
     @classmethod 
---
+++ staticmethod.py
@@ -0,0 +1,15 @@
+class Math:
+    
+    @staticmethod
+    def add5(x):
+        return x + 5
+
+    @staticmethod
+    def add10(x):
+        return x + 5
+
+
+    def pr():
+        print('run')
+
+Math.pr()
